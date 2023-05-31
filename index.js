b6acc79101dfa125bccd002251d2223920d293f6
Tim Mueller <trmueller5@gmail.com>
2023-05-31 08:37:48 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names, event){
+    const messages = [];
+    for(let x = 0; x < names.length; x++) {
+        messages.push(`Thank you, ${names[x]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+function countDown(number){
+    while(number>=0){
+        console.log(number);
+        number--;
+    }
+}
